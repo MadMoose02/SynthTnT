2db5b5987991a4e4647e3a0a8e2365583aca4908
Keshan Moosai <keshanmoosai@gmail.com>
2024-02-27 23:05:05 -0400

    minor code cleanup

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 async function main() {
-    const {readFileSync} = require('fs');
     const text2SSML = require('./text2SSML');
-    const DTTEC = require('./pronunciation');
-    const ipa2SSML = require('@theresnotime/ipa-to-ssml');
     const IBM = require('./watson');
 
-    let sentence = "Hey, there's a Phagwa celebration today"; //readFileSync('./test-ssml.xml', 'utf8');
+    let sentence = "Ah ketch a vap the other day";
     let ssml = await text2SSML.convert(sentence);
-
     console.log("SSML (TTE/C):\n" + ssml);
 
     // await IBM.speakText(sentence); // pure BRIT ENG synthesis
     await IBM.speakText(ssml, true); // synthesis with DTTEC influence
 }
-
 
 if (require.main === module) {
     console.clear();
