d7af48c1172a8f6622be306bef968e6db0874726
Keshan Moosai <keshanmoosai@gmail.com>
2024-03-25 14:40:23 -0400

    updated to web-enabled js module (uses fetch on DTTEC-API)

--- text2SSML.js
+++ text2SSML.js
@@ -1,60 +1,66 @@
-const text2IPA = require('text-to-ipa');
-const ipa2SSML = require('@theresnotime/ipa-to-ssml');
-const DTTEC    = require('./pronunciation.js');
+let DTTECEndpoint = `https://dttec-api.onrender.com/get-pronunciation`;
+let defaultXML = `
+<?xml version="1.0"?>
+<speak version="1.1"
+    xmlns="http://www.w3.org/2001/10/synthesis"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.w3.org/2001/10/synthesis
+        http://www.w3.org/TR/speech-synthesis11/synthesis.xsd"
+    xml:lang="en-US">
+    <phoneme alphabet="ipa" ph="{{{ipa}}}">{{{word}}}</phoneme>
+</speak>`;
 
-
-function containsQuestionParticle(sentence) {
-    return sentence.includes("?");
+/**
+ * This function converts the input word to SSML
+ * @param {string} word The word to convert into an SSML string
+ * @param {string} ipa The pronunciation of the word in IPA notation
+ * @returns 
+ */
+async function convertToSSML(word, ipa) {
+    try { return defaultXML.replaceAll('{{{word}}}', word).replaceAll('{{{ipa}}}', ipa); } 
+    catch (err) { return err; }
 }
 
-
-function removePunctuation(sentence) {
-    return sentence.toString().replace(/[^a-zA-Z\s]/g, '');
-}
-
-
-async function buildSSML(tags, question) {
-    let ssml = '<p><s>' + tags.join(' ');
-    return ssml + (question ? "?" : "") + "</s></p>";
-}
-
-
+/**
+ * This function iteratively converts each word in the input sentence to SSML. If a word
+ * is not found in the DTTEC dictionary (queried via the API), they will be returned as-is.
+ * @param {string} text The sentence to convert to SSML
+ * @returns The SSML string
+ */
 async function convert(text) {
-    let isQuestion = containsQuestionParticle(text);
+    let isQuestion = text.includes("?");
+    text = text.toLowerCase().replace(/[^a-zA-Z\s]/g, '');
     let tags = [];
 
     while (text.length > 0) {
         let word = text.split(' ')[0];
-        let ipa = DTTEC.lookup(removePunctuation(word.toLowerCase()));
-        if (ipa !== undefined) {
+
+        // Check if the word is in the DTTEC dictionary
+        let response = await fetch(`https://dttec-api.onrender.com/get-pronunciation/${word}`)
+            .then((response) => response.text())
+            .then((data) => JSON.parse(data));
+        
+        // Only push the word if it is in the DTTEC dictionary
+        if (response.status === 'OK') {
 
             // Convert ipa to ssml and push into tags
-            await ipa2SSML.convertToSSML(word, ipa).then((ssml) => {
+            await convertToSSML(word, response.pronunciation).then((ssml) => {
                 ssml = '<phoneme' + ssml.split('<phoneme')[1].split('</phoneme>')[0] + '</phoneme>';
                 tags.push(ssml);
             });
         
         } else { tags.push(word); }
 
-        // Remove word from the text
+        // Remove word from the text and continue to next word
         text = text.replace(word, '', 1).replace(' ', '', 1);
     }
 
-    return await buildSSML(tags, isQuestion);
+    return '<p><s>' + tags.join(' ') + (isQuestion ? "?" : "") + "</s></p>";
 }
 
-
-function main() {
-    let sentence = "Hey, it have a Phagwa celebration today";
-    convert(sentence).then((_) => console.log(`\nFinal SSML: ${_}`));
-}
-
-
-module.exports = {
-    convert
-};
-
-
-if (require.main === module) {
-    main();
-}
+// Command line (Node.js)
+// if (require.main === module) {
+//     let sentence = "Hey, it have a Phagwa celebration today";
+//     // convert(sentence).then((_) => console.log(`\nFinal SSML: ${_}`));
+//     convert(sentence);
+// }
