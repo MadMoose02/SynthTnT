9a9d2fcc71c6f0f0ecde28a8075cee5a710fd41c
Keshan Moosai <keshanmoosai@gmail.com>
2024-04-14 16:20:32 -0400

    fixed css issues and eval form

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 function toggleEvalFormModal(state) {
     evalFormModal.style.display = state ? 'block' : 'none';
     document.body.style.overflow = state ? 'hidden' : 'auto';
@@ -21,20 +23,31 @@ function populateDemographicData() {
 }
 
 function validateForm() {
+    var allChecked = true;
     for (var i = 2; i <= numQuestions; i++) {
         var radioButtons = document.getElementsByName('q' + i);
-        var radioButtonChecked = false;
         for (var x = 0; x < radioButtons.length; x++) {
-            if (radioButtons[x].checked) {
-                radioButtonChecked = true;
-                break;
+            if (radioButtons[x].checked) { 
+                document.querySelector('label[for="q' + i + '"]').classList.remove('not-answered');
+                allChecked = true;
+                break; 
             }
-        }
-        if (!radioButtonChecked || q1Input.value.trim() === '') {
-            alert('Please answer all questions before proceeding.');
-            return false;
+            allChecked = false;
+            document.querySelector('label[for="q' + i + '"]').classList.add('not-answered');
         }
     }
+
+    if (q1Input.value.trim() === '') { 
+        document.querySelector('label[for="q1"]').classList.add('not-answered');
+    } else {
+        document.querySelector('label[for="q1"]').classList.remove('not-answered');
+    }
+
+    if (!allChecked || q1Input.value.trim() === '') {
+        alert('Please answer all questions before proceeding.');
+        return false;
+    }
+
     return true;
 }
 
